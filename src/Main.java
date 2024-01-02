aaccf4593870b516e39a0819f9ea83c8bdd8ccb0
조영선 <whdudtjs5283@naver.com>
2024-01-02 17:13:25 +0900

    '#'2 레벨0, 레슨2. 가상 머신과 첫 명령어

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+/**
+ * CodeGym 과제
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Robots are friends to humans");
-        System.out.println("Humans and robots are friends forever");
-        System.out.println("Humans and robots are friends forever");
-        System.out.println("Humans and robots are friends forever");
+        solution1();
     }
+
+    /**
+     * CodeGym / Java Course / 자바 구문 / 가상 머신과 첫 명령어
+     * 레벨0, 레슨2. 가상 머신과 첫 명령어
+     *
+     * 다음 문구를 출력하는 프로그램을 작성하십시오. "아미고는 아주 영리하다".
+     *
+     * 예제 출력:
+     * 아미고는 아주 영리하다
+     * 요구 사항:
+     * •	텍스트를 출력하는 프로그램이어야 합니다.
+     * •	"아미고"라는 문구로 텍스트를 시작해야 합니다.
+     * •	"영리하다"라는 문구로 텍스트를 끝내야 합니다.
+     * •	텍스트는 공백을 포함하여 19자로 구성되어야 합니다.
+     * •	출력한 텍스트는 과제 조건과 일치해야 합니다.
+     */
+    public static void solution1() {
+        System.out.println("아미고는 아주 영리하다");
+    };
 }
