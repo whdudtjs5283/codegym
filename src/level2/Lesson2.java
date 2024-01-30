68f5009b500f301febaa5e26a40465a2ec03af19
조영선 <whdudtjs5283@naver.com>
2024-01-30 12:40:44 +0900

    10. [레벨 2, 레슨 2] 기본 데이터 유형 solution 4 ~ 6

--- src/level2/Lesson2.java
+++ src/level2/Lesson2.java
@@ -8,7 +8,103 @@ public class Lesson2 {
     public static void main(String[] args) {
 //        solution1();
 //        solution2();
-        solution3();
+//        solution3();
+//        solution4();
+//        solution5();
+        solution6();
+    }
+
+    /**
+     * In the main method, declare two int variables a and b.
+     * Assign them any values.
+     * Display their sum and product on the screen.
+     * Display each value on a new line.
+     *
+     * Example output for a = 5, b = 7:
+     * 12
+     * 35
+     * 요구 사항:
+     * •	The program should display text on the screen.
+     * •	The program should have two int variables a and b.
+     * •	The variables must be immediately assigned values.
+     * •	The main method should display the sum of the variables a and b.
+     * •	The main method should display the product of the variables a and b.
+     * •	Each value should be displayed on a new line.
+     */
+    private static void solution6() {
+        int a = 5;
+        int b = 7;
+
+        System.out.println(a + b);
+        System.out.println(a * b);
+    }
+
+    /**
+     * solution5
+     * @param a
+     */
+    public static void hackSalary(int a) {
+        a += 1000;
+        System.out.println("Your salary is: " + a + " dollars per month.");
+    }
+
+    /**
+     * In the public static void hackSalary(int a) method, increase the salary by 1000 and display the following: "Your salary is: <a+1000> dollars per month."
+     * where <a+1000> is the salary increased by 1000.
+     *
+     * Example output for a = 7000:
+     * Your salary is: 8000 dollars per month.
+     * 요구 사항:
+     * •	The program should display text on the screen.
+     * •	The main method should not call System.out.println or System.out.print.
+     * •	The hackSalary method should not return a result.
+     * •	The main method should call the hackSalary method only once.
+     * •	The hackSalary method should increase the input parameter by 1000 and output text to the screen according to the specified template.
+     */
+    private static void solution5() {
+        hackSalary(7000);
+    }
+
+    /**
+     * solution4
+     */
+    public static class Man {
+        public int age;
+        public int height;
+        public Woman wife;
+    }
+
+    /**
+     * solution4
+     */
+    public static class Woman {
+        public int age;
+        public int height;
+        public Man husband;
+    }
+
+    /**
+     * In the main method, create a Man object and save a reference to it in the variable man.
+     * Also, create a Woman object and save a reference to it in the variable woman.
+     *
+     * Hint: Use the following construct to create a Woman object and assign a reference to that object to the variable woman:
+     * VariableType variableName = new TypeOfObjectBeingCreated();
+     * Save a reference to the previously created Woman object in man.wife.
+     * Save a reference to the previously created Man object in woman.husband (Hint: woman.husband = man).
+     * 요구 사항:
+     * •	In the main method, create a Man object and save a reference to it in the variable man.
+     * •	In the main method, create a Woman object and save a reference to it in the variable woman.
+     * •	In the main method, store a reference to the previously created Woman object in man.wife.
+     * •	In the main method, store a reference to the previously created Man object in woman.husband.
+     * •	The Man class must have 3 variables.
+     * •	The Woman class must have 3 variables.
+     */
+    private static void solution4() {
+        Man man = new Man();
+        Woman woman = new Woman();
+
+        man.wife = woman;
+        woman.husband = man;
     }
 
     /**
