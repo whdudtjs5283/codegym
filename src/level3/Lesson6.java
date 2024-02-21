e05659f3a443c60565e5ee9b2defb48986bb3e58
조영선 <whdudtjs5283@naver.com>
2024-02-21 18:08:16 +0900

    #21 [레벨 3, 레슨 6] 작업 solution3 : 클래스 선언하기

--- src/level3/Lesson6.java
+++ src/level3/Lesson6.java
@@ -7,14 +7,88 @@ package level3;
 public class Lesson6 {
     public static void main(String[] args) {
 //        solution1();
-        solution2();
+//        solution2();
+        solution3();
+    }
+
+    /**
+     * solution3
+     */
+    public static class Red {
+        public Red() {
+            System.out.println("Red");
+        }
+    }
+
+    public static class Orange {
+        public Orange() {
+            System.out.println("Orange");
+        }
+    }
+
+    public static class Yellow {
+        public Yellow() {
+            System.out.println("Yellow");
+        }
+    }
+
+    public static class Green {
+        public Green() {
+            System.out.println("Green");
+        }
+    }
+
+    public static class Blue {
+        public Blue() {
+            System.out.println("Blue");
+        }
+    }
+
+    public static class Indigo {
+        public Indigo() {
+            System.out.println("Indigo");
+        }
+    }
+
+    public static class Violet {
+        public Violet() {
+            System.out.println("Violet");
+        }
+    }
+
+    /**
+     * Create 7 objects to display the 7 colors of the rainbow on the screen.
+     * Example output:
+     * Red
+     * Orange
+     * Yellow
+     * Green
+     * Blue
+     * Indigo
+     * Violet
+     * Each object displays a specific color when it is created.
+     * 요구 사항:
+     * •	The program should output text.
+     * •	You need to create 7 different objects responsible for the colors.
+     * •	The order in which the objects are created must correspond to the order of the colors in the rainbow.
+     * •	Don't change the classes responsible for the colors.
+     * •	The displayed text must match the task conditions.
+     */
+    private static void solution3() {
+        Red red = new Red();
+        Orange orange = new Orange();
+        Yellow yellow = new Yellow();
+        Green green = new Green();
+        Blue blue = new Blue();
+        Indigo Indigo = new Indigo();
+        Violet violet = new Violet();
     }
 
     /**
      * Display a 10 x 10 multiplication table in the following form:
      * 1 2 3 4 …
      * 2 4 6 8 …
-     * 3 6 9 12 …
+     * 3 6 9 12 …기
      * 4 8 12 16 …
      * …
      *
